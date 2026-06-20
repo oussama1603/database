0d2e34d2420d5cb092400264cd862d718370e3fd
oussama1603 <oussamadz1603@gmail.com>
2026-06-20 17:32:42 +0100

    Update query.sql
    learning in operator

--- query.sql
+++ query.sql
@@ -1,3 +1,4 @@
-select brand,model,price,sold from cars
-where brand in ('Ford','Chevrolet','Ferrari')
+select brand,model,condition,year from cars 
+where year in (1961,1963,1965,1967,1969)
+and condition >=3
 and sold is false;
