db1678f11b61d625e44ad7d168e791ef58c3c492
oussama1603 <oussamadz1603@gmail.com>
2026-06-21 22:14:09 +0100

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,3 +1,5 @@
 select brand,model,condition,year,price from cars
-where sold is false and condition != 5
-order by condition desc ,price;
+where color like '%red%'
+and sold is false
+order by price 
+limit 5;
