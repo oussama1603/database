9c852d93b8beeca089b5048083a6b1962c6f1548
oussama1603 <oussamadz1603@gmail.com>
2026-06-22 12:46:45 +0100

    Update query.sql
    dealing with order by

--- query.sql
+++ query.sql
@@ -1,2 +1,3 @@
-select floor(avg(price)) as average, max(price) as maximum, min(price) as minimum from cars
-where sold is true;
+select brand,count(brand),avg(price) as avg from cars
+where sold is false
+group by brand;
