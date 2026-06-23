ad36a2a5bc0a50345fbe528623e041b7827d72fa
oussama1603 <oussamadz1603@gmail.com>
2026-06-23 17:50:56 +0100

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,3 +1,5 @@
-select brand,count(brand),avg(price) as avg from cars
-where sold is false
-group by brand;
+select year,count(year) as car_count ,max(price),min(price) from cars 
+where sold is true
+group by year 
+having count(year) > 1
+order by car_count;
